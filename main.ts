49620322687115497e3d27b737d9b6cd112bfb4f
Tanner Voltage! <tannervoltage@gmail.com>
2026-08-20 10:27:00 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+namespace project {
+    // information about this project lol
+    export let name = "T-OS"
+    export let version = "indev"
+}
+console.log(`Initialising ${project.name} version ${project.version}`)
+let loadingBar = statusbars.create(scene.screenWidth(), 16, StatusBarKind.Health)
+loadingBar.bottom = scene.screenHeight()
+loadingBar.value = 0
+loadingBar.max = 4
+let loadingText = textsprite.create(`Loading ${project.name}...`)
+loadingText.left = 0
+loadingText.bottom = scene.screenHeight() - 1
